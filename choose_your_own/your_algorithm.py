685a7cb113816cbef6a6e7ca4339c41e31e276b5
Christoffer Åhrling <christoffer@nilpath.se>
2016-02-03 19:05:13 +0100

    Lesson 4

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -17,28 +17,30 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
+clf = KNeighborsClassifier(weights='uniform', algorithm='auto', n_neighbors=18)
+clf.fit(features_train, labels_train)
 
-
-
-
-
+pred = clf.predict(features_test)
 
 try:
     prettyPicture(clf, features_test, labels_test)
+    print "accuracy: {}".format(accuracy_score(pred, labels_test))
 except NameError:
     pass
